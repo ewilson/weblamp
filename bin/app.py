32f2dca470fe642d843ca4c251a77416e1af5ef9
Eric Wilson <wilson.eric.n@gmail.com>
2013-04-20 16:57:25 -0400

    closed tag, fixing bug

--- bin/app.py
+++ bin/app.py
@@ -16,9 +16,7 @@ class index:
         return render.weblamp(switch = switch)
     
     def POST(self):
-        print 'hello'
         switch = web.input()
-        print switch
         flash.switch(switch.light == 'on')
         return render.weblamp(switch = switch)
 
